e837cb5e689b7d22d4253b3057c3964cfdc0900c
Arkan <florian.bertholin@gmail.com>
2014-06-29 15:55:27 +0200

    Test part content for the sparrow test

--- mail_test.go
+++ mail_test.go
@@ -193,6 +193,8 @@ func TestSparrow(t *testing.T) {
   assert.Equal(t, mime.GetHeader("Subject"), "LOLZ3")
   assert.Equal(t, len(mime.Attachments), 0)
   assert.Equal(t, len(mime.Inlines), 0)
+  assert.Equal(t, mime.Text, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum\n\n--\nJames\n\n")
+  assert.Equal(t, mime.Html, "                <div>\n                    <span style=\"font-family: Arial, Helvetica, sans; font-size: 11px; line-height: 14px; text-align: justify;\">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>\n                </div>\n                <div><div><br></div><div>--&nbsp;</div><div>Florian Bertholin</div><div><br></div></div>\n")
 }
 
 // readMessage is a test utility function to fetch a mail.Message object.
